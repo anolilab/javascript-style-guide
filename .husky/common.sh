2943b8d6f9891d97f291a37aa941609648a1f550
prisis <d.bannert@anolilab.de>
2022-01-12 11:45:34 +0100

    chore: adding husky
    Signed-off-by: prisis <d.bannert@anolilab.de>

--- .husky/common.sh
+++ .husky/common.sh
@@ -1,10 +1,22 @@
+#!/bin/sh
+
 command_exists () {
   command -v "$1" >/dev/null 2>&1
 }
 
-# Workaround for Windows 10, Git Bash and Yarn
-if command_exists winpty && test -t 1; then
-  exec < /dev/tty
+IS_WINDOWS="false"
+
+if [ "$OSTYPE" = "cygwin" ]; then
+    IS_WINDOWS="true"
+elif [ "$OSTYPE" = "msys" ]; then
+    IS_WINDOWS="true"
+elif [ "$OSTYPE" = "win32" ]; then
+    IS_WINDOWS="true"
 fi
 
-[ -n "$CI" ] && exit 0
+# Workaround for Windows 10, Git Bash and Yarn
+if [ "$IS_WINDOWS" = "true" ]; then
+    if command_exists winpty && test -t 1; then
+        exec < /dev/tty
+    fi
+fi

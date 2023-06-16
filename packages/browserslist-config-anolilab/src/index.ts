68451f3ff686a4b10dbde105b88d40ec2d1914c2
prisis <d.bannert@anolilab.de>
2023-06-16 13:45:20 +0200

    chore(browserslist-config-anolilab): style fixes
    Signed-off-by: prisis <d.bannert@anolilab.de>

--- packages/browserslist-config-anolilab/src/index.ts
+++ packages/browserslist-config-anolilab/src/index.ts
@@ -1,15 +1,6 @@
 const config = {
-    production: [
-"> 1%",
-"last 2 versions",
-"Firefox ESR",
-"not dead",
-],
-    legacyBrowsers: [
-"> 1%",
-"last 2 versions",
-"Firefox ESR",
-],
+    production: ["> 1%", "last 2 versions", "Firefox ESR", "not dead"],
+    legacyBrowsers: ["> 1%", "last 2 versions", "Firefox ESR"],
     modernBrowsers: [
         "last 2 Chrome versions",
         "not Chrome < 60",

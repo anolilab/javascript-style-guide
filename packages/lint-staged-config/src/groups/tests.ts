9399760cbda39979692f670245c1c2bbd2a0dddf
prisis <d.bannert@anolilab.de>
2023-06-29 00:26:20 +0200

    fix: added missing jest and ava check for tests
    Signed-off-by: prisis <d.bannert@anolilab.de>

--- packages/lint-staged-config/src/groups/tests.ts
+++ packages/lint-staged-config/src/groups/tests.ts
@@ -1,8 +1,29 @@
+import { hasDependency, hasDevDependency } from "@anolilab/package-json-utils";
 import type { Config } from "lint-staged";
 
+import concatFiles from "../utils/concat-files";
+
+const hasVitest = hasDependency("vitest") || hasDevDependency("vitest");
+const hasJest = hasDependency("jest") || hasDevDependency("jest");
+const hasAva = hasDependency("ava") || hasDevDependency("ava");
+
 const group: Config = {
-    "**/__tests__/**/*.?(c|m)[jt]s?(x)": ["vitest related --run"],
-    "**/?(*.){test,spec}.?(c|m)[jt]s?(x)": ["vitest related --run"],
+    ...(hasVitest && { "**/__tests__/**/*.?(c|m)[jt]s?(x)": ["vitest related --run"], "**/?(*.){test,spec}.?(c|m)[jt]s?(x)": ["vitest related --run"] }),
+    ...(hasJest && {
+        "**/*.spec.{js,ts,tsx}": ["jest --findRelatedTests"],
+        "**/*.test.{js,ts,tsx}": ["jest --findRelatedTests"],
+        "**/test/*.{js,ts,tsx}": ["jest --findRelatedTests"],
+        "**/__mocks__/*.{js,ts,tsx}": ["jest --findRelatedTests"],
+        "**/__tests__/*.{js,ts,tsx}": ["jest --findRelatedTests"],
+        "**/__tests__/**/*.?(c|m)[jt]s?(x)": ["jest --findRelatedTests"],
+        "**/?(*.){test,spec}.?(c|m)[jt]s?(x)": ["jest --findRelatedTests"],
+    }),
+    ...(hasAva && {
+        "**/test.js": (filenames: string[]) => [`ava ${concatFiles(filenames)}`],
+        "**/test-*.js": (filenames: string[]) => [`ava ${concatFiles(filenames)}`],
+        "**/*.(spec|test).js": (filenames: string[]) => [`ava ${concatFiles(filenames)}`],
+        "**/(test|tests|__tests__)/**/*.js": (filenames: string[]) => [`ava ${concatFiles(filenames)}`],
+    }),
 };
 
 export default group;

d461ea1cb985b94de2935073fc0506604b817ebf
prisis <d.bannert@anolilab.de>
2023-09-21 16:28:23 +0200

    ci: fixed node version check
    Signed-off-by: prisis <d.bannert@anolilab.de>

--- verify-node-version.cjs
+++ verify-node-version.cjs
@@ -3,13 +3,13 @@ let requiredVersion = require("node:fs").readFileSync(".nvmrc", { encoding: "utf
 if (!requiredVersion.includes("v")) {
     requiredVersion = `v${requiredVersion}`;
 }
-
-if (process.env.SKIP_CHECK !== undefined) {
+// eslint-disable-next-line dot-notation
+if (process.env["SKIP_CHECK"] !== undefined) {
     // eslint-disable-next-line unicorn/no-process-exit
     process.exit(0);
 }
 
-if (process.version.split(".")[0] !== requiredVersion.split(".")[0]) {
+if (!(requiredVersion.split(".")[0] <= process.version.split(".")[0])) {
     console.error(`[!] This project requires Node.js ${requiredVersion}, current version is ${process.version}`);
 
     // eslint-disable-next-line unicorn/no-process-exit

c899a830080f139afb83fd8e2b7fb4bfc9aba774
prisis <d.bannert@anolilab.de>
2023-09-25 23:54:22 +0200

    fix: Handle failed promise in removeIgnoredFiles function
    Error handling is added in removeIgnoredFiles function in ESLint config to better address instances when a promise fails. Instead of just directly awaiting on the promises sent by the `eslint.isPathIgnored()` function, the `Promise.allSettled()` method is now being used. This allows us to wait until all promises are settled, then check each promise's status. If a promise fails, an appropriate error message is output, and the process is exited. This prevents the promise-rejection-unhandled exception and ensures more robust functioning of the lint-stage-config.
    
    Signed-off-by: prisis <d.bannert@anolilab.de>

--- packages/lint-staged-config/src/groups/eslint/remove-ignored-files.ts
+++ packages/lint-staged-config/src/groups/eslint/remove-ignored-files.ts
@@ -6,7 +6,17 @@ import isWindows from "../../utils/is-windows";
 const removeIgnoredFiles = async (filenames: string[]): Promise<string[]> => {
     const eslint = new ESLint();
     // eslint-disable-next-line compat/compat
-    const ignoredFiles = await Promise.all(filenames.map(async (filename) => await eslint.isPathIgnored(filename)));
+    const promises = await Promise.allSettled(filenames.map(async (filename) => await eslint.isPathIgnored(filename)));
+    const ignoredFiles = promises.map((promise) => {
+        if (promise.status === "fulfilled") {
+            return promise.value;
+        }
+
+        console.error(`\nUnable to determine if file is ignored.\n\n\n${promise.reason}`);
+
+        throw new Error("Stopping lint-staged because of an error.");
+    });
+
     // eslint-disable-next-line security/detect-object-injection
     const filteredFiles = filenames.filter((_, index) => !ignoredFiles[index]);
 

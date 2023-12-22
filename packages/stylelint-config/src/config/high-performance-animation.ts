933e269f0f967873c6fd7913e959a288c0983fb7
prisis <d.bannert@anolilab.de>
2023-12-22 22:33:34 +0100

    chore: downgrade stylelint 15.11.0
    Signed-off-by: prisis <d.bannert@anolilab.de>

--- packages/stylelint-config/src/config/high-performance-animation.ts
+++ packages/stylelint-config/src/config/high-performance-animation.ts
@@ -2,7 +2,7 @@ const config = {
     // prevent use of low performance animation and transition properties.
     plugins: ["stylelint-high-performance-animation"],
     rules: {
-        'plugin/no-low-performance-animation-properties': true,
+        "plugin/no-low-performance-animation-properties": true,
     },
 };
 

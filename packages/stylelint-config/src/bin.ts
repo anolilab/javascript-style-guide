244eeb7dc41599bdf2a03fabd5ce33454866effa
prisis <d.bannert@anolilab.de>
2024-12-16 21:42:06 +0100

    fix(stylelint-config): removed @anolilab/package-json-utils, using cwd of the command call
    Signed-off-by: prisis <d.bannert@anolilab.de>

--- packages/stylelint-config/src/bin.ts
+++ packages/stylelint-config/src/bin.ts
@@ -1,28 +1,18 @@
-import { existsSync, writeFile } from "node:fs";
+import { existsSync } from "node:fs";
+import { readFile, writeFile } from "node:fs/promises";
 import { join } from "node:path";
-import { env, exit } from "node:process";
-import { promisify } from "node:util";
-
-import { packageIsTypeModule, projectPath } from "@anolilab/package-json-utils";
-
-if (env["CI"] !== undefined) {
-    exit(0);
-}
-
-const writeFileAsync = promisify(writeFile);
-
-console.log("Configuring @anolilab/stylelint-config", projectPath, "\n");
+import { exit } from "node:process";
 
 const file = ".stylelintrc";
 
 /**
  * Writes .stylelintrc.cjs if it doesn't exist. Warns if it exists.
  */
-const writeStylelintRc = async () => {
+const writeStylelintRc = async (cwd: string, isTypeModule: boolean) => {
     // eslint-disable-next-line no-restricted-syntax,no-loops/no-loops
     for (const filename of [file, `${file}.js`, `${file}.cjs`, `${file}.json`, `${file}.yaml`, `${file}.yml`, `stylelint.config.js`, `stylelint.config.cjs`]) {
         // eslint-disable-next-line security/detect-non-literal-fs-filename
-        if (existsSync(join(projectPath, filename))) {
+        if (existsSync(join(cwd, filename))) {
             console.warn(
                 '⚠️  .stylelintrc.js already exists; Make sure that it includes the following for @anolilab/stylelint-config to work as it should: { "extends": ["@anolilab/stylelint-config"] }.',
             );
@@ -31,8 +21,8 @@ const writeStylelintRc = async () => {
         }
     }
 
-    const stylelintPath = join(projectPath, ".stylelintrc.js");
-    const content = `${packageIsTypeModule ? "export default" : "module.exports ="} {
+    const stylelintPath = join(cwd, ".stylelintrc.js");
+    const content = `${isTypeModule ? "export default" : "module.exports ="} {
     "extends": [
         "@anolilab/stylelint-config",
     ]
@@ -40,14 +30,14 @@ const writeStylelintRc = async () => {
 
 `;
 
-    await writeFileAsync(stylelintPath, content, "utf-8");
+    await writeFile(stylelintPath, content, "utf-8");
 };
 
 /**
  * Writes .stylelintignore if it doesn't exist. Warns if it exists.
  */
-const writeStylelintIgnore = async () => {
-    const stylelintIgnorePath = join(projectPath, ".stylelintignore");
+const writeStylelintIgnore = async (cwd: string) => {
+    const stylelintIgnorePath = join(cwd, ".stylelintignore");
     const content = `package.json
 package-lock.json
 yarn.lock
@@ -63,14 +53,30 @@ dist/**
         return;
     }
 
-    await writeFileAsync(stylelintIgnorePath, content, "utf-8");
+    await writeFile(stylelintIgnorePath, content, "utf-8");
 };
 
 // eslint-disable-next-line unicorn/prefer-top-level-await
 (async () => {
+    const cwd = process.cwd();
+
+    const packageJsonPath = join(cwd, "package.json");
+
+    if (!existsSync(packageJsonPath)) {
+        console.error("😬  No package.json found in the current directory. You need to run this command in a directory with a package.json file.");
+
+        exit(1);
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const packageJson = JSON.parse(await readFile(packageJsonPath, "utf-8"));
+
+    console.log("Configuring @anolilab/stylelint-config", cwd, "\n");
+
     try {
-        await writeStylelintRc();
-        await writeStylelintIgnore();
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        await writeStylelintRc(cwd, packageJson?.type === "module");
+        await writeStylelintIgnore(cwd);
 
         console.log("😎  Everything went well, have fun!");
 

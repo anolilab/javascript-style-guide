58f3d94088f71be1b11812a69a2c0516600f28a4
prisis <d.bannert@anolilab.de>
2024-12-16 21:57:24 +0100

    chore: updated dependencies, cleanup after deprecation of some packages
    Signed-off-by: prisis <d.bannert@anolilab.de>

---
+++ taze.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "taze";
+
+export default defineConfig({
+    // ignore packages from bumping
+    exclude: [
+        "@types/node",
+        "eslint",
+        "eslint-plugin-vitest",
+    ],
+    ignorePaths: ["node_modules", "packages/**/dist", "packages/**/coverage", "packages/**/__fixtures__", "packages/**/__tests__"],
+    mode: "latest",
+    recursive: true,
+    // write to package.json
+    write: true,
+});

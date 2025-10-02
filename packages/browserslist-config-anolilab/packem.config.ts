9bac090854ae699438b2ae164ffa7a540cacbf4a
prisis <d.bannert@anolilab.de>
2025-10-02 23:40:30 +0200

    refactor: remove isolatedDeclarationTransformer from browserslist-config-anolilab configuration

--- packages/browserslist-config-anolilab/packem.config.ts
+++ packages/browserslist-config-anolilab/packem.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from "@visulima/packem/config";
-import isolatedDeclarationTransformer from "@visulima/packem/dts/isolated/transformer/typescript";
 import transformer from "@visulima/packem/transformer/esbuild";
 
 export default defineConfig({
     cjsInterop: true,
-    isolatedDeclarationTransformer,
     node10Compatibility: {
         typeScriptVersion: ">=5.0",
         writeToPackageJson: true,
